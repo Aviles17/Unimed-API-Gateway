7fc8b2b179ba250d0104166f0e72269d0baeb6c6
Aviles17 <santiaviles17@gmail.com>
2023-11-10 02:31:43 -0500

    Añadir template para JWT con Firebase ID

--- api_server.py
+++ api_server.py
@@ -1,19 +1,16 @@
 from flask import Flask, request, jsonify
 import requests
 from requests.exceptions import Timeout
-#import firebase_config
-#import pyrebase
-
-
-
+import firebase_config
+import pyrebase
 
 app = Flask(__name__)
 
 NaiveBayesServer = 'http://localhost:8081/NaiveBayesPredict'
 ClusterServer = 'http://localhost:8082/'
 
-#firebase = pyrebase.initialize_app(config)
-#auth = firebase.auth()
+firebase = pyrebase.initialize_app(firebase_config.config)
+auth = firebase.auth()
 
 @app.route("/", methods=['GET'])
 def root_server():
@@ -21,34 +18,40 @@ def root_server():
 
 @app.route('/normalflow', methods=['POST'])
 def normal_flow():
-    
-    data = request.get_json()
     id_token = request.headers.get('Authorization')
     
-    if data:
+    if id_token:
         try:
-            #Enviar datos a el servidor Flask de procesamiento de Emails
-            response1 = requests.post(NaiveBayesServer, json=data, timeout=5)
-        except Timeout:
-            return jsonify({"error": "The request to NaiveBayes Server timed out"}), 408
-        
-        #Si la respuesta del servidor es correcta y la prediccion es 1 entonces pasar al clusterizado
-        if response1.status_code == 200 and response1.text == "1.0":
-            try:
-                response2 = requests.post(ClusterServer, json=request.get_json(), timeout=5)
-            except Timeout:
-                return jsonify({"error": "The request to Cluster Server timed out"}), 408
+            decoded_token = auth.verify_id_token(id_token)
+            uid = decoded_token['uid']
             
-            return jsonify(response2.json()), response2.status_code
-        
-        #Si la predicción es 0 entonces retornar el status
-        else:
-            return jsonify(response1.json()), response1.status_code    
+            data = request.get_json()
+            
+            if data:
+                try:
+                    #Enviar datos a el servidor Flask de procesamiento de Emails
+                    response1 = requests.post(NaiveBayesServer, json=data, timeout=5)
+                except Timeout:
+                    return jsonify({"error": "The request to NaiveBayes Server timed out"}), 408
+                
+                #Si la respuesta del servidor es correcta y la prediccion es 1 entonces pasar al clusterizado
+                if response1.status_code == 200 and response1.text == "1.0":
+                    try:
+                        response2 = requests.post(ClusterServer, json=request.get_json(), timeout=5)
+                    except Timeout:
+                        return jsonify({"error": "The request to Cluster Server timed out"}), 408
+                    
+                    return jsonify(response2.json()), response2.status_code
+                
+                #Si la predicción es 0 entonces retornar el status
+                else:
+                    return jsonify(response1.json()), response1.status_code    
+            else:
+                return jsonify({"error": "Bad Request"}), 400
+        except:
+            return jsonify({"error": "Invalid ID token"}), 401
     else:
-        return jsonify({"error": "Bad Request"}), 400
-    
-        
-        
+        return jsonify({"error": "Missing ID token"}), 401
 
 if __name__ == '__main__':
     #Run Flask app
